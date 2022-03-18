29990b795ab75569c4d596ef737d39751ca891aa
lavender <lavender@LAPTOP-2PBMQB2G>
2022-03-18 21:39:56 +0800

    homework deal

--- homework/testwork.m
+++ homework/testwork.m
@@ -1,7 +1,18 @@
 A=[1,2,3,4;5,6,7,8;9,10,11,12];
 B(4,5)=200;
-A=A(:,2);
+disp (A);
+disp (B);
+
+A(:,[1,3])=[];
 disp (A);
 
-B=B([1,2,4],:);
+B(3,:)=[];
 disp (B);
+
+C='I''m A STUDENT.';
+cc=find(C>='A'&C<='Z');
+C(cc)=C(cc)-('A'-'a');
+disp (C);
+
+p=strrep(C,'student','teacher');
+disp (p);
