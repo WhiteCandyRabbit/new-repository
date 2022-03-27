c8ebfc22adacf18e62fa435f8c44377e2061dc9d
lavender <lavender@LAPTOP-2PBMQB2G>
2022-03-27 11:18:22 +0800

    3.27 correct

--- homework/testwork.m
+++ homework/testwork.m
@@ -1,18 +1,22 @@
 A=[1,2,3,4;5,6,7,8;9,10,11,12];
-B(4,5)=200;
+B=[1,2;3,4;5,6;7,8;9,10];
 disp (A);
 disp (B);
 
 A(:,[1,3])=[];
 disp (A);
 
-B(3,:)=[];
+%B(3,:)=[];此处进行对比，在CSDN上找到倒数第n行的表示方法是：end-（n-1）
+B(end-2,:)=[];
 disp (B);
 
+%此处进行大小写替换，运用函数find
 C='I''m A STUDENT.';
+disp(C);
 cc=find(C>='A'&C<='Z');
 C(cc)=C(cc)-('A'-'a');
 disp (C);
 
+%此处进行单词替换，运用函数strrep
 p=strrep(C,'student','teacher');
 disp (p);
